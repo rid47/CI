8ce31c46f7e812cbe325b8e9906996bbacacc5f5
rid47 <ridwanmizan@gmail.com>
2018-03-05 15:08:04 +0600

    Add files via upload

---
+++ application/controllers/Home.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+        
+        //load model
+        $this->load->model('home_model');
+    }
+        
+    public function index()
+    {
+        //get data from the database
+        $data['images'] = $this->home_model->get_images();
+        
+        //load view and pass the data
+        $this->load->view('home_view', $data);
+    }
+}
